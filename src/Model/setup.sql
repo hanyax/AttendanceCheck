df70317984ff6e67b16d8b4b4bb326f474e8dd8d
Shawn Xu <hanyax@cs.washington.edu>
2017-08-22 13:54:53 +0800

    fixed sql setup file

--- src/Model/setup.sql
+++ src/Model/setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS test(
+CREATE TABLE IF NOT EXISTS normal_record(
   date DATE,
   name CHAR(20),
   arrive TIMESTAMP(6) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS test(
   UNIQUE (date, name)
 ) CHARACTER SET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS abnormal (
+CREATE TABLE IF NOT EXISTS abnormal_record(
   date DATE,
   name CHAR(20),
   time TIMESTAMP(2) NOT NULL,
